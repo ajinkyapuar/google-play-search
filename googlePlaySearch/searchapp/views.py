7484d88f51c2b05d0bd3bb8767e803419c2b642f
Ajinkya <albus702@gmail.com>
2017-05-23 13:55:02 +0530

    templating setup

--- googlePlaySearch/searchapp/views.py
+++ googlePlaySearch/searchapp/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
+    template = loader.get_template('index.html')
     return HttpResponse("Hello! Search App initialized")
