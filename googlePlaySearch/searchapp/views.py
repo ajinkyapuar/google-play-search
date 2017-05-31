def36672c3fad9f257f17799aa7db297dcd10e8d
Ajinkya <albus702@gmail.com>
2017-05-31 20:51:44 +0530

    test

--- googlePlaySearch/searchapp/views.py
+++ googlePlaySearch/searchapp/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 # from django.shortcuts import render
-
+#
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
