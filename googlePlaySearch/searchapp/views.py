93b22b33f1c47854a68f6388e838b1f2be55fda8
Ajinkya <albus702@gmail.com>
2017-05-31 19:25:03 +0530

    fixes, cleaning

--- googlePlaySearch/searchapp/views.py
+++ googlePlaySearch/searchapp/views.py
@@ -25,9 +25,8 @@ def index(request):
             'error': 'Search Term cannot be empty',
         }
         return HttpResponse(template.render(context))
-    # print(len(query.split(" ")))
     if len(query.split(" ")) >= 2:
-        print("****** Query can containt only one word ******")
+        print("****** Query can contain only one word ******")
         context = {
             'error': 'Search Term should be one word only',
         }
@@ -56,7 +55,7 @@ def index(request):
 def scrapeGooglePlayStore(query, qId):
     print("****** Scrapping Google Play Store ******")
     webRequest = requests.get('https://play.google.com/store/search?q=' + query)
-    print(webRequest.status_code)
+    # print(webRequest.status_code)
     if webRequest.status_code == requests.codes.ok:
         page = webRequest.text
         soup = BeautifulSoup(page)
@@ -84,93 +83,3 @@ def details(request, pk):
     results = Results.objects.filter(id=pk)
     context = {'details': results}
     return HttpResponse(template.render(context))
-
-#################################################################################################################################
-
-
-# if request.GET:
-#     print("****** REQUEST FOUND ******")
-#     query = request.GET.get('query')
-#     # print query
-#     # try:
-#     queries = Queries.objects.filter(query_text=query)
-#     # print queries
-#     if not queries:
-#         print("****** QUERY NOT FOUND!!! ******")
-#         q = Queries(query_text=query, pub_date=timezone.now())
-#         q.save()
-#         # print q.id
-#         webRequest = requests.get('https://play.google.com/store/search?q=' + query)
-#         if webRequest.status_code == requests.codes.ok:
-#             page = webRequest.text
-#             soup = BeautifulSoup(page)
-#             cards = soup.find_all('div', {"class": "card"})
-#             # count = 1
-#             for card in cards[:10]:
-#                 print("********* Begin App Data *********")
-#                 # print count
-#                 # print "AppID: " + card['data-docid']
-#                 # print "AppName: " + card.find('a', {"class": "title"}).text
-#                 # print "DeveloperName: " + card.find('a', {"class": "subtitle"}).text
-#                 # count+=1
-#                 # print q.id
-#    #########################
-#                 # r = Results(query_id_id=q.id, app_id=card['data-docid'],
-#                 #             app_name=card.find('a', {"class": "title"}).text,
-#                 #             dev_name=card.find('a', {"class": "subtitle"}).text)
-#                 # r.save()
-#   #########################
-#                 print("********* End App Data *********")
-#     else:
-#         print("****** QUERY FOUND!!! ******")
-#         # queries = Queries.objects.filter(query_text=query)
-#         # print queries
-#         # print [q.id for q in Queries.objects.filter(query_text=query)]
-#         q_id = [q.id for q in queries]
-#         # q_text = [q.query_text for q in queries]
-#         # print q_id[0]
-#         # print q_text[0]
-#         results = Results.objects.filter(query_id=q_id[0])
-#         # print results
-#         if not results:
-#             print("****** RESULTS NOT FOUND!!! ******")
-#       ####################
-#             # webRequest = requests.get('https://play.google.com/store/search?q=' + query)
-#             # if webRequest.status_code == requests.codes.ok:
-#             #     page = webRequest.text
-#             #     soup = BeautifulSoup(page)
-#             #     cards = soup.find_all('div', {"class": "card"})
-#             #     # count = 1
-#             #     for card in cards[:10]:
-#             #         print("********* Begin App Data *********")
-#             #         # print count
-#             #         # print "AppID: " + card['data-docid']
-#             #         # print "AppName: " + card.find('a', {"class": "title"}).text
-#             #         # print "DeveloperName: " + card.find('a', {"class": "subtitle"}).text
-#             #         # count+=1
-#             #         # print q.id
-#             #         r = Results(query_id_id=q.id, app_id=card['data-docid'],
-#             #                     app_name=card.find('a', {"class": "title"}).text,
-#             #                     dev_name=card.find('a', {"class": "subtitle"}).text)
-#             #         r.save()
-#             #         print("********* End App Data *********")
-#             #
-#        ####################
-#
-#         else:
-#             print("****** RESULTS FOUND!!! ******")
-#             # context = {
-#             #     'app_list': results,
-#             # }
-#             # for cr in results:
-#             #     print cr
-#     context = {
-#         'app_list': results,
-#     }
-#     # except Queries.DoesNotExist:
-#     #     print("****** Query doesn't exsist! ******")
-#     # print context
-#     # return render_to_response('index.html', {'app_list': results})
-#     return HttpResponse(template.render(context, request))
-# # return render_to_response('index.html', {})
-# return HttpResponse(template.render(context, request))
